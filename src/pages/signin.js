1ed3ecd964b9db4b6404fc5f905d838623d68fe9
davillafer <davillafer@gmail.com>
2021-07-28 17:13:06 +0200

    Main page with routes

---
+++ src/pages/signin.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { FooterContainer } from "../containers/footer";
+
+export default function Signin() {
+    return (
+        <>
+            <FooterContainer />
+        </>
+    );
+}
